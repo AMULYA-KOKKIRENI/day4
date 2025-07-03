04f087d2e8fbf76a4052f9de1ab7be282039c039
AMULYA-KOKKIRENI <amulyakokkireni@gmail.com>
2025-07-03 18:58:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+function App(){
+  return(
+    <div className="product">
+      <h1>shopye</h1>
+      <section>
+        <h3>phone</h3>
+        <div className="hii">
+        <img src="./download.png"alt="local-product"></img>
+        <p>its having great features</p>
+        <p>price:2000</p>
+        <h3>phone15</h3>
+        <div className="hii">
+        <img src="./download.png"alt="local-product"></img>
+        <p>its having great features</p>
+        <p>price:2500</p>
+        <h3>phone17</h3>
+        <div className="hii">
+        <img src="./download.png"alt="local-product"></img>
+        <p>its having great features</p>
+        <p>price:2800</p>
+        <h3>laptop</h3>
+        <div className="hii">
+        <img src="./download1.png"alt="local-product"></img>
+        <p>its having great features</p>
+        <p>price:4800</p>
+  
+        <button>Add to cart</button>
+        </div>
+        </div>
+        </div>
+        </div>
+      </section>
+    </div>
+    
+  );
+}
+export default App;
